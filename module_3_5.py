491d7c0835e6c9de37edcf4e4288bac79650421a
AntonB44 <baidakov.an44@gmail.com>
2024-07-22 22:49:58 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -4,7 +4,10 @@
 
 def get_multiplied_digits(number):
     str_number = str(number)
-    first = int(str_number[0])
+    if int(str_number[0]) != 0:
+        first = int(str_number[0])
+    else:
+        first = 1
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
@@ -12,12 +15,14 @@ def get_multiplied_digits(number):
 
 
 result = get_multiplied_digits(40203)
-result1 = get_multiplied_digits(304)
-result2 = get_multiplied_digits(340)
+result1 = get_multiplied_digits(407)
+result2 = get_multiplied_digits(740)
+result3 = get_multiplied_digits(9)
 print(result)
 print(result1)
 print(result2)
+print(result3)
 
 print('При тестировании обнаружилось, что если передать в функцию')
 print('число, оканчивающееся на 0, то в результате будет 0.')
-print('Так и задумывалось или нужно доработать?')
+print('По рекомендации преподавателя была сделана доработка')
